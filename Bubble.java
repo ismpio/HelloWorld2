a1917e40970d63db5b2f3786fbeb7fea0d2a2480
ismpio <93166105+ismpio@users.noreply.github.com>
2023-04-12 22:52:47 -0400

    Update Bubble.java

--- Bubble.java
+++ Bubble.java
@@ -56,13 +56,7 @@ public class Bubble
         for(int i=0; i < randArray.length; i++){
             System.out.print(randArray[i] + " ");
         }
-/**
-        System.out.println("Number of Command Line Argument = "+args.length);
 
-        for(int i = 0; i< args.length; i++) {
-            System.out.println(String.format("Command Line Argument %d is %s", i, args[i]));
-        }
- */
 
     }
 }
